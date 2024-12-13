653674a8d63cadaf6cfb5561aeb716d6005bfb42
liu-jia-hao <jiahao@fetalmedicine.tw>
2024-12-13 10:46:16 +0800

    FirebaseUI working

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,16 +37,15 @@ export default defineConfig<Credentials>({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] }
     },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] }
+    // },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] }
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] }
-    }
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] }
+    // }
   ],
   webServer: {
     command: 'npm run start',
